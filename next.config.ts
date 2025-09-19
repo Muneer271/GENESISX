a99782a60bf4dd26cb9491b072ab100d34bab099
Madara Uchiha <madarauchiha99007@gmail.com>
2025-09-19 23:50:25 +0000

    Try fixing this error: `Runtime Error: Error: ENOENT: no such file or di

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverComponentsExternalPackages: ['@genkit-ai/firebase', '@genkit-ai/googleai', 'genkit'],
 };
 
 export default nextConfig;
